0015335f49fff64aa6b0ca2fbb1ea0dd76f30989
Alex Kondratenko <kopernik@inbox.ru>
2024-11-07 12:18:44 +0200

    Add new services registration in Startup.cs

--- LandonHotel/Startup.cs
+++ LandonHotel/Startup.cs
@@ -24,6 +24,7 @@ namespace LandonHotel
             services.AddMvc();
             services.AddTransient<IRoomService, RoomService>();
             services.AddTransient<IBookingService, BookingService>();
+            services.AddTransient<IBookingsRepository, BookingsRepository>();
             services.AddTransient<IRoomsRepository, RoomsRepository>();
         }
 
