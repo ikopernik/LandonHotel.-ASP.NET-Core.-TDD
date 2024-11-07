3c58b15a0ce628b4ec7af2622c0bc96c80ea61d6
Alex Kondratenko <kopernik@inbox.ru>
2024-11-07 12:01:13 +0200

    Move 4 tests for pets into 1 Theory.

--- LandonHotel.Tests/BookingServiceTests.cs
+++ LandonHotel.Tests/BookingServiceTests.cs
@@ -47,80 +47,28 @@ namespace LandonHotel.Tests
             Assert.False(isValid);
         }
 
-        [Fact]
-        public void IsBookingValid_PetsNotAllowed_Invalid()
+        [Theory]
+        [InlineData(false, true, false)]
+        [InlineData(false, false, true)]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, true)]
+
+        public void IsBookingValid_Pets(bool areAllowed, bool hasPets, bool result)
         {
             var service = Subject();
             _roomsRepo.Setup(x => x.GetRoom(1)).Returns(new Room
             {
-                ArePetsAllowed = false
+                ArePetsAllowed = areAllowed
             });
 
             var isValid = service.IsBookingValid(
                 1,
                 new Booking
                 {
-                    HasPets = true
+                    HasPets = hasPets
                 });
 
-            Assert.False(isValid);
-        }
-
-        [Fact]
-        public void IsBookingValid_NoPetsNotAllowed_IsValid()
-        {
-            var service = Subject();
-            _roomsRepo.Setup(x => x.GetRoom(1)).Returns(new Room
-            {
-                ArePetsAllowed = false
-            });
-
-            var isValid = service.IsBookingValid(
-                1,
-                new Booking
-                {
-                    HasPets = false
-                });
-
-            Assert.True(isValid);
-        }
-
-        [Fact]
-        public void IsBookingValid_NoPetsAllowed_IsValid()
-        {
-            var service = Subject();
-            _roomsRepo.Setup(x => x.GetRoom(1)).Returns(new Room
-            {
-                ArePetsAllowed = true
-            });
-
-            var isValid = service.IsBookingValid(
-                1,
-                new Booking
-                {
-                    HasPets = false
-                });
-
-            Assert.True(isValid);
-        }
-
-        [Fact]
-        public void IsBookingValid_PetsAllowed_IsValid()
-        {
-            var service = Subject();
-            _roomsRepo.Setup(x => x.GetRoom(1)).Returns(new Room
-            {
-                ArePetsAllowed = true
-            });
-
-            var isValid = service.IsBookingValid(
-                1,
-                new Booking
-                {
-                    HasPets = true
-                });
-
-            Assert.True(isValid);
+            Assert.Equal(isValid, result);
         }
     }
 }
